38eaa4c1993b296696cd887d32194059a55dab64
Mirosław Pragłowski <m@praglowski.com>
2016-08-30 17:50:54 +0200

    RES 0.12.1 format of publishing events

--- orders/lib/orders/order.rb
+++ orders/lib/orders/order.rb
@@ -13,44 +13,44 @@ module Orders
       @fee_calculator = fee_calculator
     end
 
-    def item(sku:, quantity:, net_price:, vat_rate:)
+    def add_item(sku:, quantity:, net_price:, vat_rate:)
       raise NotAllowed unless @state == :draft
-      apply(OrderItemAdded.new(
-        order_number: number,
+      apply(OrderItemAdded.new(data: {
+        order_number: id,
         sku: sku,
         quantity: quantity,
         net_price: net_price,
-        vat_rate: vat_rate))
+        vat_rate: vat_rate}))
     end
 
     def submit(customer_id:)
       raise NotAllowed unless @state == :draft
       raise Invalid    if @items.empty?
       net_total = calculate_net_total
-      apply(OrderSubmitted.new(
-        order_number: number,
+      apply(OrderSubmitted.new(data: {
+        order_number: id,
         customer_id:  customer_id,
-        items:        items,
+        items:        @items,
         net_total:    net_total,
-        fee:          fee_calculator.call(net_total)))
+        fee:          @fee_calculator.call(net_total)}))
     end
 
     def cancel
       raise NotAllowed unless [:draft, :submitted].include?(@state)
-      apply(OrderCancelled.new(
-        order_number: number))
+      apply(OrderCancelled.new(data: {
+        order_number: id}))
     end
 
     def expire
       raise NotAllowed unless @state == :draft
-      apply(OrderExpired.new(
-        order_number: number))
+      apply(OrderExpired.new(data: {
+        order_number: id}))
     end
 
     def ship
       raise NotAllowed unless @state == :submitted
-      apply(OrderShipped.new(
-        order_number: number))
+      apply(OrderShipped.new(data: {
+        order_number: id}))
     end
 
     private
