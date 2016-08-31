3804d7ba6f42522e4a2cfabe7bda69095e5311ed
Mirosław Pragłowski <m@praglowski.com>
2016-08-31 17:35:46 +0200

    Add more domain events matchers, avoid checking metadata (for now)

--- orders/spec/spec_helper.rb
+++ orders/spec/spec_helper.rb
@@ -5,14 +5,19 @@ require_relative '../lib/orders'
 
 require 'rspec/expectations'
 
+def map_events(events)
+  events.map{|ev| {class: ev.class, data: ev.data.to_h }}
+end
+
 RSpec::Matchers.define :publish do |expected_events|
-
-  def map_events(events)
-    events.map{|ev| {class: ev.class, data: ev.data.to_h, metadata: ev.metadata.to_h}}
-  end
-
   match do |aggregate|
     actual_events = aggregate.unpublished_events
+    expect(map_events(actual_events)).to eq map_events(expected_events)
+  end
+end
+
+RSpec::Matchers.define :have_events do |expected_events|
+  match do |actual_events|
     expect(map_events(actual_events)).to eq map_events(expected_events)
   end
 end
