c12dca2555be905d41ab05eb898f45bd24c9b6e1
Mirosław Pragłowski <m@praglowski.com>
2016-09-09 11:11:53 +0200

    Add handlers to take care of order's read model

---
+++ app/services/read_model/order_cancelled_handler.rb
@@ -0,0 +1,9 @@
+module ReadModel
+  class OrderCancelledHandler
+    def call(ev)
+      order = Order.find_by(number: ev.data.order_number)
+      order.state = 'cancelled'
+      order.save!
+    end
+  end
+end
