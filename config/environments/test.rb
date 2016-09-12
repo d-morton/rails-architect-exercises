bbec84eac7be1c4b6eb1bf5470035d3e093c142d
Szymon Fiedler <fidelski@gmail.com>
2016-09-12 13:21:39 +0200

    Dropped InMemoryRepository since it's not cleaned

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.event_store = RailsEventStore::Client.new(
-    repository: RailsEventStore::InMemoryRepository.new)
 end
