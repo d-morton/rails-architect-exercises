354620d7d4e3ddd7a66ff8f88696d216940336e4
Robert Pankowecki <robert.pankowecki@gmail.com>
2016-09-17 21:35:28 +0200

    Remove the ability to delete customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -51,16 +51,6 @@ class CustomersController < ApplicationController
     end
   end
 
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :payments, only: [:create]
 
-  resources :customers
+  resources :customers, only: [:index, :show, :new, :edit, :create, :update]
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
