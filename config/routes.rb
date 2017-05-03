cd692de6cd83fe196d887b12021bfbf2d2c143c1
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2017-05-03 17:56:35 +0200

    remove useless comment

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
 
   resources :customers, only: [:index, :show, :new, :edit, :create, :update]
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
